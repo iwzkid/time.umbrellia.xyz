9307afaea2ed8d9e2ec2f7d61ce0c66545647901
Lia <lia@xkid.ro>
2019-05-12 22:14:59 +0300

    created pdo, created display notes function, calling function in the notes_display.php

--- inc/db_functions.php
+++ inc/db_functions.php
@@ -1,2 +1,48 @@
 <?php
 #DB RELATED FUNCTIONS
+
+#DB_data
+$host = 'localhost';
+$user = 'root';
+$password = 'root';
+$db = 'time_umbrellia_xyz';
+
+//DSN
+$dsn = 'mysql:host='. $host .';dbname='. $db;
+
+//PDO_instance
+$pdo = new PDO($dsn, $user, $password);
+
+//SELECT & DISPLAY EVENTS
+$stmt = $pdo->query('SELECT * FROM events_table');
+
+//SELECT & DISPLAY NOTES
+
+function display_notes() {
+    global $pdo;
+
+    $stmt = $pdo->query('SELECT * FROM notes_table');
+
+        echo '<table class="notes-table">';
+
+        echo '<thead>';
+        echo '<th><h5>Notes</h5></th><th><h5>Date</h5></th>';
+        echo '</thead>';
+
+        echo '<tbody>';
+
+        while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+
+            //$increase = '<a href="?increase='.$row['id'].'">+</a>';
+            //$delete = '<a href="?delete='.$row['id'].'">x</a>';
+
+            echo '<tr>';
+            echo '<td class="notes_column">' . $row['notes'] .'</td>';
+            echo '<td class="notes_time">' . $row['date_time'] . '</td>';
+            echo '</tr>';
+
+        } 
+
+        echo '</tbody>';
+        echo '</table>';     
+}
