2c84385552fa86b7e3ba6b85dfcb45bdb1c0fb6e
Lia <lia@xkid.ro>
2019-06-03 15:36:32 +0300

    added columns in the admin view

--- inc/login/add_calendar.php
+++ inc/login/add_calendar.php
@@ -3,47 +3,56 @@
 ?>
 
 <div class="container-fluid">
-    <hr>
-    <h5>Add event to agenda:</h5>
-    <form class="form-group" method="POST">
-        Name:
-        <br>
-        <input type="text" name="name_event" placeholder="name"/>
-        <br>
-        Location:
-        <br>
-        <input type="text" name="location_event" placeholder="location"/> 
-        <br>
-        Date & Time:
-        <br>
-      <!--  <input type="date" name="time_event" placeholder="date and time"/> -->
-    <div class="input-group date" id="datetimepicker2" data-target-input="nearest">
-        <input type="text" name="time_event" class="form-control datetimepicker-input" data-target="#datetimepicker2"/>
-        <div class="input-group-append" data-target="#datetimepicker2" data-toggle="datetimepicker">
-            <div class="input-group-text"><i class="fa fa-calendar"></i></div>
+<hr>
+    <div class="row">
+        <div class="col-md-6"> 
+            <h5>Add event to agenda:</h5>
+            <form class="form-group" method="POST">
+                Name:
+                <br>
+                <input type="text" name="name_event" placeholder="name"/>
+                <br>
+                Location:
+                <br>
+                <input type="text" name="location_event" placeholder="location"/> 
+                <br>
+                Date & Time:
+                <br>
+            <!--  <input type="date" name="time_event" placeholder="date and time"/> -->
+            <div class="input-group date" id="datetimepicker2" data-target-input="nearest">
+                <input type="text" name="time_event" class="form-control datetimepicker-input" data-target="#datetimepicker2"/>
+                <div class="input-group-append" data-target="#datetimepicker2" data-toggle="datetimepicker">
+                    <div class="input-group-text"><i class="fa fa-calendar"></i></div>
+                </div>
+            </div>
+
+        <!--testin-->
+
+        <!--no more testin-->
+
+                <br>
+                Duration:
+                <br>
+                <input type="text" name="duration_event" placeholder="duration of event"/> 
+                <br>
+                Important:
+                <br>
+                <input type="checkbox" name="important_event"/> 
+                <br>
+                Recurring:
+                <br>
+                <input type="text" name="recurring_event" placeholder="reccurs for"/> 
+                <br>
+                <br>
+                <button type="submit" class="btn btn-success">Add</button>
+                <br>
+            </form>
+        </div>
+
+        <div class="col-md-6">
+            <h5>Guidelines for adding an event:</h5>
         </div>
     </div>
-<!--testin-->
-
-<!--no more testin-->
-
-        <br>
-        Duration:
-        <br>
-        <input type="text" name="duration_event" placeholder="duration of event"/> 
-        <br>
-        Important:
-        <br>
-        <input type="checkbox" name="important_event"/> 
-        <br>
-        Recurring:
-        <br>
-        <input type="text" name="recurring_event" placeholder="reccurs for"/> 
-        <br>
-        <br>
-        <button type="submit" class="btn btn-success">Add</button>
-        <br>
-    </form>
 </div>
 
 
--- inc/login/add_notes.php
+++ inc/login/add_notes.php
@@ -4,26 +4,35 @@
 
 <div class="container-fluid">
     <hr>
-    <h5>Add note:</h5>
-    <form method="POST">
-        Note:
-        <br>
-        <input type="text" name="note_text" placeholder="note"/>
-        <br>
-        Valid until:
-        <br>
-        <!-- <input type="date" name="valid_until" placeholder="display until"/> -->
-        <div class="input-group date" id="datetimepicker1" data-target-input="nearest">
-            <input type="text" name="valid_until" class="form-control datetimepicker-input" data-target="#datetimepicker1"/>
-            <div class="input-group-append" data-target="#datetimepicker1" data-toggle="datetimepicker">
-                <div class="input-group-text"><i class="fa fa-calendar"></i></div>
-            </div>
+    <div class="row">
+        <div class="col-md-6">
+            <h5>Add note:</h5>
+            <form class="form-group" method="POST">
+                Note:
+                <br>
+                <input type="text" name="note_text" placeholder="note"/>
+                <br>
+                Valid until:
+                <br>
+                <!-- <input type="date" name="valid_until" placeholder="display until"/> -->
+                <div class="input-group date" id="datetimepicker1" data-target-input="nearest">
+                    <input type="text" name="valid_until" class="form-control datetimepicker-input" data-target="#datetimepicker1"/>
+                    <div class="input-group-append" data-target="#datetimepicker1" data-toggle="datetimepicker">
+                        <div class="input-group-text"><i class="fa fa-calendar"></i></div>
+                    </div>
+                </div>
+                <br>
+                <br>
+                <button type="submit" class="btn btn-success">Add</button>
+                <br>
+            </form>
         </div>
-        <br>
-        <br>
-        <button type="submit" class="btn btn-success">Add</button>
-        <br>
-    </form>
+
+        <div class="col-md-6">
+            <h5>Guidelines for adding a note:</h5>
+        </div>
+
+    </div>
     <hr>
 </div>
 
