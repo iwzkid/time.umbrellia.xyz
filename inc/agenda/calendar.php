85f1e81fb6a010ff90319e55942d71f1308815ef
Lia <lia@xkid.ro>
2019-06-27 19:39:30 +0300

    fixing paddings for mobile devices in agenda

--- inc/agenda/calendar.php
+++ inc/agenda/calendar.php
@@ -3,52 +3,52 @@
 ?>
 
 <div class="container_fluid" id="calendar">
-        <div class="row">
+    <div class="row pl-2 pr-2">
         <div class="col-md-12 col-sm-10">
 
-    <?php 
+            <?php 
 
-    $daysofweek = array(
-        'Monday' => array(),
-        'Tuesday' => array(),
-        'Wednesday' => array(),
-        'Thursday' => array(),
-        'Friday' => array(),
-        'Saturday' => array(),
-        'Sunday' => array()
-    );
+            $daysofweek = array(
+                'Monday' => array(),
+                'Tuesday' => array(),
+                'Wednesday' => array(),
+                'Thursday' => array(),
+                'Friday' => array(),
+                'Saturday' => array(),
+                'Sunday' => array()
+            );
 
-    if($_GET['week'] == 'prev'){
-        $query = "SELECT *, DAYNAME(date_time) 'day' FROM events_table WHERE WEEK(date_time) = WEEK(NOW() - INTERVAL 7 DAY)";
-    }elseif($_GET['week'] == 'next'){
-        $query =  "SELECT *, DAYNAME(date_time) 'day' FROM events_table WHERE WEEK(date_time) = WEEK(NOW() + INTERVAL 7 DAY)";
-    }else{
-        $query = "SELECT *, DAYNAME(date_time) 'day' FROM events_table WHERE WEEK(date_time) = WEEK(NOW())";
-    }
+            if($_GET['week'] == 'prev'){
+                $query = "SELECT *, DAYNAME(date_time) 'day' FROM events_table WHERE WEEK(date_time) = WEEK(NOW() - INTERVAL 7 DAY)";
+            }elseif($_GET['week'] == 'next'){
+                $query =  "SELECT *, DAYNAME(date_time) 'day' FROM events_table WHERE WEEK(date_time) = WEEK(NOW() + INTERVAL 7 DAY)";
+            }else{
+                $query = "SELECT *, DAYNAME(date_time) 'day' FROM events_table WHERE WEEK(date_time) = WEEK(NOW())";
+            }
 
-    $stmt = $pdo->query($query); 
+            $stmt = $pdo->query($query); 
 
-    while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-        $daysofweek[ $row['day'] ][] = $row; 
-    } 
+            while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+                $daysofweek[ $row['day'] ][] = $row; 
+            } 
 
-    foreach($daysofweek as $dayname => $events){ ?>
-        <div class="row">
-            <div class="dayname">
-                <h5><?php echo $dayname ?>:</h5>
-                <hr>
-            </div>
-            <?php if(!empty($events)){ 
-                foreach($events as $event){
-                    echo $event['name'].' '.$event['location'].' '.format_datetime($event['date_time']); 
-                    echo '<br><hr>';
-                }
-             } else {
-                echo 'There are no events for this day.';
-                echo '<br><hr>';
-            } ?>
-        </div>
-    <?php } ?>
+            foreach($daysofweek as $dayname => $events){ ?>
+                <div class="row">
+                    <div class="dayname">
+                        <h5><?php echo $dayname ?>:</h5>
+                        <hr>
+                    </div>
+                    <?php if(!empty($events)){ 
+                        foreach($events as $event){
+                            echo $event['name'].' '.$event['location'].' '.format_datetime($event['date_time']); 
+                            echo '<br><hr>';
+                        }
+                    } else {
+                        echo 'There are no events for this day.';
+                        echo '<br><hr>';
+                    } ?>
+                </div>
+            <?php } ?>
 
         </div>
     </div>
